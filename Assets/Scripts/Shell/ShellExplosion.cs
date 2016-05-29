185d104670450ed65aa39b1689a6120079aead93
Marcio Vinicius dos Santos <mvs.usp@gmail.com>
2016-05-29 14:16:17 -0400

    added shell logic

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -16,16 +16,51 @@ public class ShellExplosion : MonoBehaviour
         Destroy(gameObject, m_MaxLifeTime);
     }
 
-
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
-    }
+        var colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
 
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody)
+            {
+                continue;
+            }
+
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            if (!targetHealth)
+            {
+                continue;
+            }
+
+            var damage = CalculateDamage(targetRigidbody.position);
+            targetHealth.TakeDamage(damage);
+        }
+
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.duration);
+        Destroy(gameObject);
+    }
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+        var explosionToTarget = targetPosition - transform.position;
+        var explosionDistance = explosionToTarget.magnitude;
+        var relativeDistance = (m_ExplosionRadius - explosionDistance)/m_ExplosionRadius;
+        var damage = relativeDistance*m_MaxDamage;
+
+        damage = Mathf.Max(damage, 0);
+        return damage;
     }
 }
